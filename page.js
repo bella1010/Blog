8d74d8306aff75ede5cd1c6bf145b16f8f6477ff
BellaChen <bella.chen@cecport.com>
2018-01-23 11:00:10 +0800

    Signed-off-by: BellaChen <bella.chen@cecport.com>

--- page.js
+++ page.js
@@ -6,6 +6,16 @@ var pageData = [
             {"title": "倒计时 ", "url": "TimeButton/index.html", "author": 1, "update": "2017-07-05", "redmine": "", "isnew": 1},
             {"title": "固定边栏的效果 ", "url": "Sider/index.html", "author": 1, "update": "2017-07-05", "redmine": "", "isnew": 1}
         ]
+    },
+    {
+        "dir": "componentDevelopmentMode", "title": "组件开发方式", "items": [
+            {"title": "componentDevelopmentMode", "url": "demo1.html", "author": 1, "update": "2017-12-20", "redmine": "", "isnew": 1},
+            {"title": "componentDevelopmentMode", "url": "demo2.html", "author": 1, "update": "2017-12-20", "redmine": "", "isnew": 1},
+            {"title": "componentDevelopmentMode", "url": "demo3.html", "author": 1, "update": "2017-12-20", "redmine": "", "isnew": 1},
+            {"title": "componentDevelopmentMode", "url": "demo4.html", "author": 1, "update": "2017-12-20", "redmine": "", "isnew": 1},
+            {"title": "componentDevelopmentMode", "url": "demo5.html", "author": 1, "update": "2017-12-20", "redmine": "", "isnew": 1},
+            {"title": "componentDevelopmentMode", "url": "demo6.html", "author": 1, "update": "2017-12-20", "redmine": "", "isnew": 1}
+        ]
     }
 
 
